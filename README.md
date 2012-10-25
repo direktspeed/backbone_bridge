ece90ccea9b42150675169d76747a16fb7327fad
Austin McDaniel <amcdaniel2@gmail.com>
2012-10-25 10:23:07 -0500

    moving from amcdaniel to jupiterjs

---
+++ README.md
@@ -0,0 +1,20 @@
+----
+# Backbone Bridge
+----
+
+Backbone Bridge is a one-to-one mapper for users migrating from [BackboneJS](http://backbonejs.org/) to 
+[CanJS](http://canjs.us/).  No code changes!
+
+## Why!?
+
+Why would you wanna upgrade? CanJS offers:
+
+- Faster Performance
+- Richer Feature set
+- Lighter-weight download
+
+
+## How?
+
+You just drop in _bridge.js_, remove Backbone from your application, add CanJS and now all your code is running
+on CanJS without any syntax changes.
---
+++ bridge.js
@@ -0,0 +1,104 @@
+steal('can/control', 
+	  'can/model',
+	  'can/observe',
+	  'can/observe/list',
+	  'can/util/string')
+.then(function(Control, Model){
+	
+	window.Backbone = {};
+	window._ = {};
+
+	// ========================== Underscore ===========================
+
+	_.extend = can.extend;
+
+	var idCounter = 0,
+		uniqueId = function(prefix) {
+	    	var id = idCounter++;
+	    	return prefix ? prefix + id : id;
+	  	};
+
+	Backbone.noConflict = $.noConflict;
+
+	// ============================= Events =============================
+
+	Backbone.Events = {};
+	Backbone.Events.on = null;
+	Backbone.Events.off = null;
+	Backbone.Events.trigger = null;
+
+	// =========================== Collection ==========================
+
+	Backbone.Collection = can.Observe.List;
+	Backbone.Collection.prototype.add = can.Observe.List.push;
+
+	// ============================== View =============================
+
+	Backbone.View = can.Control;
+	Backbone.View.prototype.template = can.view;
+
+	// ============================= Model =============================
+
+	Backbone.Model = can.Model;
+	Backbone.Model.initialize = can.Model.init;
+	Backbone.Model.defaults = can.Model.defaults;
+
+	can.extend(can.Observe.prototype, {
+		on:function(event,cb){
+			var self = this,
+				split = event.split(':');
+
+			this.bind(split[split.length - 1], function(ev, newVal, oldVal){
+				cb(self, arguments[split.length === 1 ? 3 : 1]);
+			});
+		}
+	});
+
+	var old = can.Model.prototype.setup;
+	can.extend(can.Model.prototype, {
+
+		setup:function(){
+			this.cid = uniqueId('c');
+			old.apply(this,arguments);
+		},
+
+		clear:function(attrs){
+			var attrs = this.attr();
+			for(var i in attrs){
+				this.attr(attrs, undefined);
+			}
+		},
+
+		escape:function(attr){
+			return can.esc(this.attr(attr));
+		},
+
+		has:function(attr){
+			return this.attr(attr) != undefined;
+		},
+
+		unset:function(attr){
+			// Remove an attribute by deleting it from the internal attributes 
+			// hash. Fires a "change" event unless silent is passed as an option.
+		},
+
+		toJSON:function(){
+			return can.toJSON(this.attr());
+		},
+
+		parse:function(json){
+			return can.parseJSON(json);
+		}
+	});
+	
+	Backbone.Model.prototype.idAttribute = can.Model.id;
+	Backbone.Model.prototype.on = can.Observe.prototype.on;
+	Backbone.Model.prototype.set = can.Model.prototype.attr;
+	Backbone.Model.prototype.get = can.Model.prototype.attr;
+	Backbone.Model.prototype.attributes = can.Model.prototype.attr;
+
+	$.each(['isNew','clear','escape','has','unset','toJSON', 'save', 'destroy', 'cid', 'parse'],function(i,func){
+		Backbone.Model.prototype[func] = can.Model.prototype[func];
+	});
+
+});
